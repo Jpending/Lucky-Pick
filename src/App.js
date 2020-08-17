dd94b6a137b667bbed57377be7afa731116d74b0
John Pendergast <jpending@gmail.com>
2020-08-17 18:26:13 -0400

    added some basic functionality, next implement random number generation, and displaying of numbers to play

--- src/App.js
+++ src/App.js
@@ -1,45 +1,79 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MyNumbers from './MyNumbers';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Lucky Pick</h1>
-      </header>
-      <body>
-        <label for="integer">Put  a number you want to use here</label>
-        <input name="integer" type="number" />
-        <button type="submit">Add to My Numbers</button>
-        <h2>My Numbers</h2>
-        <ul>
-          <li>My number</li>
-          <li>Random 1</li>
-          <li>Random 2</li>
-          <li>Random 3</li>
-          <li>Random 4</li>
-          <li>Random 5</li>
-        </ul>
+class App extends React.Component {
+  state = {
+    currentNumber: null,
+    myNumbers: [],
+    generatedNums: [],
+    Currentlyplaying: '',
+    isLucky: false
+  }
 
-        <h3>Currently Playing:</h3>
-        <select>
-          <option>Pick 3</option>
-          <option>Pick 4</option>
-          <option>Jersey Cash 5</option>
-          <option>Pick 6</option>
-          <option>Quick Draw</option>
-          <option>CASH POP</option>
-          <option>Powerball</option>
-          <option>Mega Millions</option>
-          <option>CASH4LIFE</option>
-          <option>5 Card Cash</option>
-          <option>FAST PLAY</option>
-        </select>
-      </body>
-    </div>
-  );
+  handleSelectGame(currentGame) {
+    console.log(currentGame);
+    const game = currentGame;
+    this.setState({ Currentlyplaying: game })
+  }
+
+  handleGetLucky() {
+    console.log(`Prepare to get lucky`);
+    this.setState({ isLucky: !this.state.isLucky })
+  }
+
+  handleAddNumber(number) {
+    console.log(`adding ${number} to your numbers`);
+    if (number) {
+      if (this.state.myNumbers.includes(number)) {
+        alert('Can not add duplicate numbers!')
+      }
+      else { this.setState({ myNumbers: [...this.state.myNumbers, number] }) }
+    } else { alert('Can not add an empty number!') }
+  }
+
+  handleCurrentNumber(number) {
+    console.log(number);
+    this.setState({ currentNumber: number })
+  }
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Lucky Pick</h1>
+        </header>
+        <div>
+          <label >Put  a number you want to use here</label>
+          <input name="integer" type="number" onChange={(e) => this.handleCurrentNumber(e.currentTarget.value)} />
+          <button type="submit" onClick={() => { this.handleAddNumber(this.state.currentNumber) }}>Add to My Numbers</button>
+          {this.state.myNumbers.length > 0 && <button type="submit" onClick={() => this.handleGetLucky()}>Get Lucky</button>}
+          {this.state.isLucky === true && <div><h2>My Lucky Numbers</h2><ul><MyNumbers numbers={this.state.myNumbers} /></ul></div>}
+
+
+          <h3>Currently Playing: {this.state.Currentlyplaying}</h3>
+          <select onChange={(e) => this.handleSelectGame(e.currentTarget.value)} >
+            <option value="">Select</option>
+            <option value="Pick 3">Pick 3</option>
+            <option value="Pick 4">Pick 4</option>
+            <option value="Jersey Cash 5">Jersey Cash 5</option>
+            <option value="Pick 6">Pick 6</option>
+            <option value="Quick Draw">Quick Draw</option>
+            <option value="CASH POP">CASH POP</option>
+            <option value="Powerball">Powerball</option>
+            <option value="Mega Million">Mega Millions</option>
+            <option value="CASH4LIFE">CASH4LIFE</option>
+            <option value="5 Card Cash">5 Card Cash</option>
+            <option value="FAST PLAY">FAST PLAY</option>
+          </select>
+        </div>
+      </div >
+    );
+  }
 }
 
 export default App;
---
+++ src/MyNumbers.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function MyNumbers(props) {
+  const { numbers } = props;
+  return (
+
+    numbers.map(number => <li key={number + 'myNums'}>{number}</li>)
+  )
+}
