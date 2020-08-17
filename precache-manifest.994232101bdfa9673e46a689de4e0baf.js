477d159bca8bb30826b279866cde0f66caadd752
John Pendergast <jpending@gmail.com>
2020-08-17 17:18:42 -0400

    Updates

---
+++ precache-manifest.994232101bdfa9673e46a689de4e0baf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6770d331d78517a8ee9ad9dd89acd162",
+    "url": "/Lucky-Pick/index.html"
+  },
+  {
+    "revision": "0dc91a045d777e567a63",
+    "url": "/Lucky-Pick/static/css/main.e6ae0c26.chunk.css"
+  },
+  {
+    "revision": "6744be7626167efebb5a",
+    "url": "/Lucky-Pick/static/js/2.d149f106.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Lucky-Pick/static/js/2.d149f106.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dc91a045d777e567a63",
+    "url": "/Lucky-Pick/static/js/main.2768a24a.chunk.js"
+  },
+  {
+    "revision": "9fea2e8c600a1bafef86",
+    "url": "/Lucky-Pick/static/js/runtime-main.74562582.js"
+  },
+  {
+    "revision": "b955bca5db8809776a778a86fe434c8e",
+    "url": "/Lucky-Pick/static/media/logo.b955bca5.svg"
+  }
+]);
